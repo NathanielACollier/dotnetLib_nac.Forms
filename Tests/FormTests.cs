b9f51d2f1b4c22ded26cf4c342d236f35d2e77f1
Nathaniel Collier <nathaniel.collier@aecc.com>
2018-10-06 11:31:15 -0500

    Starting tests, and adding the Avalonia.Desktop nuget package

---
+++ Tests/FormTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class FormTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
