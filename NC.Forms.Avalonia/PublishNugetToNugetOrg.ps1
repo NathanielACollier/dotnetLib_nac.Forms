8a29823eff66171e07955f4c399f95c267acb456
Nathaniel Collier <drxs3v3n@gmail.com>
2021-03-04 12:09:11 -0600

    Start work to make changes to publish to nuget.org
    Will need to change the namespace to make it work with nuget.org

---
+++ NC.Forms.Avalonia/PublishNugetToNugetOrg.ps1
@@ -0,0 +1,20 @@
+﻿$buildConfigurationName = "Debug"
+
+
+$scriptParentFolderPath = [System.IO.Path]::GetDirectoryName( $MyInvocation.InvocationName )
+
+$buildPath = [system.io.path]::Combine( $scriptParentFolderPath, "bin", $buildConfigurationName)
+$nugetPackages = Get-ChildItem $buildPath -Filter *.nupkg
+$package = $nugetPackages | Sort-Object -Descending -Property CreationTIme | Select-Object -First 1
+
+$settings = (Get-Content '~/settings.json' | Out-String | ConvertFrom-Json)
+
+<#
+Example from: https://docs.microsoft.com/en-us/nuget/nuget-org/publish-a-package
+dotnet nuget push AppLogger.1.0.0.nupkg --api-key <put key here> --source https://api.nuget.org/v3/index.json
+
+#>
+
+& dotnet @("nuget","push", $package.FullName, "--api-key", $settings.nugetOrgAPIKey, "--source",
+         "https://api.nuget.org/v3/index.json" 
+ )
