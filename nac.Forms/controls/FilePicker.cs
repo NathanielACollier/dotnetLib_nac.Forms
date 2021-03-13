52f2a88101490a9761b8fa4d2da16d59dff7b348
Nathaniel Collier <nathaniel.a.collier@gmail.com>
2021-03-13 12:50:20 -0600

    notes

--- nac.Forms/controls/FilePicker.cs
+++ nac.Forms/controls/FilePicker.cs
@@ -6,6 +6,10 @@ using Avalonia.Markup.Xaml;
 /*
  Followed user control guide documentation here:
  https://avaloniaui.net/docs/tutorial/creating-a-view
+ 
+ There is good information on an example of the OpenFileDialog here:
+ https://stackoverflow.com/questions/56604439/filedialog-opens-on-loop-using-openfiledialog-on-avalonia
+ 
  */
 
 namespace nac.Forms.controls
