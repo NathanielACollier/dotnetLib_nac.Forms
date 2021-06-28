dc0b8b1cd8dc7d8f82397f31c9ef6e6b00048e2a
Nathaniel Collier <drxs3v3n@gmail.com>
2021-06-28 10:34:13 -0500

    Documenting where the animation came from

--- nac.Forms/controls/LoadingIndicatorText.xaml.cs
+++ nac.Forms/controls/LoadingIndicatorText.xaml.cs
@@ -3,6 +3,9 @@ using Avalonia.Markup.Xaml;
 
 namespace nac.Forms.controls
 {
+    /*
+     Original marquee animation came from here: https://github.com/AvaloniaUI/Avalonia/issues/1263
+     */
     public class LoadingIndicatorText : UserControl
     {
         public LoadingIndicatorText()
