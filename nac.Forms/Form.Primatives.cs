8c71ffcc9f4956fcc2cbab28c06e7823833527eb
Nathaniel A Collier <drxs3v3n@gmail.com>
2022-10-15 14:04:53 -0500

    Cleanup:Move empty bitmap
    Move the empty bitmap creation into an ImageHelpers

--- nac.Forms/Form.Primatives.cs
+++ nac.Forms/Form.Primatives.cs
@@ -216,20 +216,7 @@ namespace nac.Forms
 
                                                     if (imgData == null)
                                                     {
-                                                        /*
-                                                         Created an empty bitmap with mspaint, then extracted out the bytes from it by reading the file like this
-                                                            var data = System.IO.File.ReadAllBytes(@"C:\Users\nathaniel\Desktop\Untitled.bmp");
-                                                            string.Join(',', data.Select(b=> b.ToString())).Dump();
-                                                         */
-                                                        
-                                                        var emptyBitmapData = new byte[]
-                                                        {
-                                                            66, 77, 66, 0, 0, 0, 0, 0, 0, 0, 62, 0, 0, 0, 40, 0, 0, 0,
-                                                            1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 4, 0, 0, 0,
-                                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                                            255, 255, 255, 0, 128, 0, 0, 0
-                                                        };
-                                                        imgData = emptyBitmapData;
+                                                        imgData = lib.ImageHelpers.CreateEmptyBitmapImage();
                                                     }
                                                     
                                                     using (var ms = new System.IO.MemoryStream(imgData))
---
+++ nac.Forms/lib/ImageHelpers.cs
@@ -0,0 +1,26 @@
+﻿namespace nac.Forms.lib;
+
+public class ImageHelpers
+{
+    public static byte[] CreateEmptyBitmapImage()
+    {
+        /*
+         Created an empty bitmap with mspaint, then extracted out the bytes from it by reading the file like this
+            var data = System.IO.File.ReadAllBytes(@"C:\Users\nathaniel\Desktop\Untitled.bmp");
+            string.Join(',', data.Select(b=> b.ToString())).Dump();
+         */
+                                                        
+        var emptyBitmapData = new byte[]
+        {
+            66, 77, 66, 0, 0, 0, 0, 0, 0, 0, 62, 0, 0, 0, 40, 0, 0, 0,
+            1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 4, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            255, 255, 255, 0, 128, 0, 0, 0
+        };
+
+        return emptyBitmapData;
+    }
+    
+    
+    
+}
