cc4991a7f02d6f293f43a69cef6fa72386ab04d5
Nathaniel Collier <nathaniel.a.collier@gmail.com>
2022-11-21 17:50:01 -0600

    test:Move Image testing

---
+++ TestApp/lib/TestFunctionGroups/Image.cs
@@ -0,0 +1,60 @@
+﻿using nac.Forms;
+using nac.Forms.model;
+
+namespace TestApp.lib.TestFunctionGroups;
+
+public class Image
+{
+    
+    
+    private static void Empty(Form f)
+    {
+        f.Model["img"] = null;
+
+        f.Text("Display no image")
+            .Image("img");
+    }
+    
+    
+    
+    private static void EmbdedResource(Form f)
+    {
+        f.Model["playIcon"] = lib.Resources.GetImage("TestApp.resources.playIcon.png");
+
+        f.Text("Embeded Resource Test")
+            .Image("playIcon");
+    }
+    
+    
+    private static void FromWebURL(Form f)
+    {
+        // default the URL
+        f.Model["url"] = "https://preview.redd.it/p9o46zrc21m81.jpg?auto=webp&s=f2f9c88a8e91c836f2b6377d5c900b416f02d62f";
+        f.Text("Image from URL")
+            .HorizontalGroup(hg =>
+            {
+                hg.Text("URL: ")
+                    .TextBoxFor("url",
+                        style: new Style
+                        {
+                            width = 350
+                        })
+                    .Button("Go", async () =>
+                    {
+                        string url = f.Model["url"] as string;
+                        // download it and convert it to an avalonia Bitmap
+                        using (var client = new System.Net.WebClient())
+                        {
+                            f.Model["img"] = client.DownloadData(url);
+                        }
+                    })
+                    .Button("Clear", async () =>
+                    {
+                        f.Model["img"] = null;
+                    });
+            })
+            .Image(modelFieldName: "img");
+    }
+    
+    
+}
--- TestApp/lib/TestFunctions.cs
+++ TestApp/lib/TestFunctions.cs
@@ -26,6 +26,7 @@ public static class TestFunctions
         {
             typeof(TestFunctionGroups.Button),
             typeof(TestFunctionGroups.TreeView),
+            typeof(TestFunctionGroups.Image),
             typeof(TestFunctions)
         };
         
@@ -994,59 +995,6 @@ public static class TestFunctions
             log.info("After show dialog is displayed");
         });
     }
-
-    public static void TestImage_FromWebURL(Form f)
-    {
-        // default the URL
-        f.Model["url"] = "https://preview.redd.it/p9o46zrc21m81.jpg?auto=webp&s=f2f9c88a8e91c836f2b6377d5c900b416f02d62f";
-        f.Text("Image from URL")
-            .HorizontalGroup(hg =>
-            {
-                hg.Text("URL: ")
-                    .TextBoxFor("url",
-                        style: new Style
-                        {
-                            width = 350
-                        })
-                    .Button("Go", async () =>
-                    {
-                        string url = f.Model["url"] as string;
-                        // download it and convert it to an avalonia Bitmap
-                        using (var client = new System.Net.WebClient())
-                        {
-                            f.Model["img"] = client.DownloadData(url);
-                        }
-                    })
-                    .Button("Clear", async () =>
-                    {
-                        f.Model["img"] = null;
-                    });
-            })
-            .Image(modelFieldName: "img");
-    }
     
-    
-    public static void TestImage_Empty(Form f)
-    {
-        f.Model["img"] = null;
-
-        f.Text("Display no image")
-            .Image("img");
-    }
-
-    
-
-
-
-    public static void TestImage_EmbdedResourceTest(Form f)
-    {
-        f.Model["playIcon"] = lib.Resources.GetImage("TestApp.resources.playIcon.png");
-
-        f.Text("Embeded Resource Test")
-            .Image("playIcon");
-    }
-
-
-
     
 }
