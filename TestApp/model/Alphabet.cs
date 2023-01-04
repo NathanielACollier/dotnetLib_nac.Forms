7db23cd4b63d81496c6531858d12ac827f2fbefb
Nathaniel A Collier <drxs3v3n@gmail.com>
2023-01-03 19:34:37 -0600

    Move the rest of the test functions in here

---
+++ TestApp/model/Alphabet.cs
@@ -0,0 +1,160 @@
+namespace TestApp.model;
+
+public class Alphabet : nac.Forms.model.ViewModelBase
+{
+    public string A
+    {
+        get { return GetValue(() => A); }
+        set { SetValue(() => A, value); }
+    }
+
+    public string B
+    {
+        get { return GetValue(() => B); }
+        set { SetValue(() => B, value); }
+    }
+
+    public string C
+    {
+        get { return GetValue(() => C); }
+        set { SetValue(() => C, value); }
+    }
+
+    public string D
+    {
+        get { return GetValue(() => D); }
+        set { SetValue(() => D, value); }
+    }
+
+    public string E
+    {
+        get { return GetValue(() => E); }
+        set { SetValue(() => E, value); }
+    }
+
+    public string F
+    {
+        get { return GetValue(() => F); }
+        set { SetValue(() => F, value); }
+    }
+
+    public string G
+    {
+        get { return GetValue(() => G); }
+        set { SetValue(() => G, value); }
+    }
+
+    public string H
+    {
+        get { return GetValue(() => H); }
+        set { SetValue(() => H, value); }
+    }
+
+    public string I
+    {
+        get { return GetValue(() => I); }
+        set { SetValue(() => I, value); }
+    }
+
+    public string J
+    {
+        get { return GetValue(() => J); }
+        set { SetValue(() => J, value); }
+    }
+
+    public string K
+    {
+        get { return GetValue(() => K); }
+        set { SetValue(() => K, value); }
+    }
+
+    public string L
+    {
+        get { return GetValue(() => L); }
+        set { SetValue(() => L, value); }
+    }
+
+    public string M
+    {
+        get { return GetValue(() => M); }
+        set { SetValue(() => M, value); }
+    }
+
+    public string N
+    {
+        get { return GetValue(() => N); }
+        set { SetValue(() => N, value); }
+    }
+
+    public string O
+    {
+        get { return GetValue(() => O); }
+        set { SetValue(() => O, value); }
+    }
+
+    public string P
+    {
+        get { return GetValue(() => P); }
+        set { SetValue(() => P, value); }
+    }
+
+    public string Q
+    {
+        get { return GetValue(() => Q); }
+        set { SetValue(() => Q, value); }
+    }
+
+    public string R
+    {
+        get { return GetValue(() => R); }
+        set { SetValue(() => R, value); }
+    }
+
+    public string S
+    {
+        get { return GetValue(() => S); }
+        set { SetValue(() => S, value); }
+    }
+
+    public string T
+    {
+        get { return GetValue(() => T); }
+        set { SetValue(() => T, value); }
+    }
+
+    public string U
+    {
+        get { return GetValue(() => U); }
+        set { SetValue(() => U, value); }
+    }
+
+    public string V
+    {
+        get { return GetValue(() => V); }
+        set { SetValue(() => V, value); }
+    }
+
+    public string W
+    {
+        get { return GetValue(() => W); }
+        set { SetValue(() => W, value); }
+    }
+
+    public string X
+    {
+        get { return GetValue(() => X); }
+        set { SetValue(() => X, value); }
+    }
+
+    public string Y
+    {
+        get { return GetValue(() => Y); }
+        set { SetValue(() => Y, value); }
+    }
+
+    public string Z
+    {
+        get { return GetValue(() => Z); }
+        set { SetValue(() => Z, value); }
+    }
+}
