82680bda2e857b99e6af4e70ac651c447d5349e9
Nathaniel A Collier <drxs3v3n@gmail.com>
2023-03-06 17:46:09 -0600

    Start with a copy of the dropdown control

--- nac.Forms/Form.Collections.cs
+++ nac.Forms/Form.Collections.cs
@@ -134,5 +134,71 @@ namespace nac.Forms
         }
 
 
+
+        public Form Autocomplete<T>(string itemSourceModelName,
+            string selectedItemModelName,
+            Action<T> onSelectionChanged = null,
+            Action<Form> populateItemRow = null,
+            model.Style style = null)
+        {
+            var tb = new Avalonia.Controls.AutoCompleteBox();
+            lib.styleUtil.style(this, tb, style);
+            
+            // Just as a safety check, make them init the model first
+            if( !(getModelValue(itemSourceModelName) is IEnumerable<T>))
+            {
+                throw new Exception($"Model {nameof(itemSourceModelName)} source property specified by name [{itemSourceModelName}] must be a IEnumerable<T>");
+            }
+            
+            // item source binding
+            AddBinding<IEnumerable>(modelFieldName: itemSourceModelName,
+                control: tb,
+                property: Avalonia.Controls.AutoCompleteBox.ItemsProperty,
+                isTwoWayDataBinding:false);
+            
+            // selected item binding
+            AddBinding<object>(modelFieldName: selectedItemModelName,
+                control: tb,
+                property: Avalonia.Controls.AutoCompleteBox.SelectedItemProperty,
+                isTwoWayDataBinding:true);
+            
+            if (populateItemRow != null)
+            {
+                tb.ItemTemplate = new FuncDataTemplate<object>((itemModel, nameScope) =>
+                {
+                    if (itemModel == null)
+                    {
+                        // how could itemModel be null?  Sometimes it is though, so strange
+                        var tb = new Avalonia.Controls.TextBlock();
+                        tb.Text = "(null)";
+                        return tb;
+                    }
+                    
+                    var rowForm = new Form(__app: this.app, _model: new lib.BindableDynamicDictionary());
+                    // this has to have a unique model
+                    rowForm.DataContext = itemModel;
+                    populateItemRow(rowForm);
+
+                    rowForm.Host.DataContext = itemModel;
+
+                    return rowForm.Host;
+                });
+            }
+
+            tb.SelectionChanged += (_s, _args) =>
+            {
+                if (_args.AddedItems.OfType<T>().Any())
+                {
+                    var first = _args.AddedItems.Cast<T>().First();
+                    onSelectionChanged?.Invoke(first);
+                }
+            };
+            
+            AddRowToHost(tb);
+            return this;
+            
+        }
+
+
 	}
 }
