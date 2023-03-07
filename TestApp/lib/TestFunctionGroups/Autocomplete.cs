ed289ccfd26af7172cfa04d181063e9b43a2a73f
Nathaniel A Collier <drxs3v3n@gmail.com>
2023-03-06 18:01:15 -0600

    Add example with static string list

---
+++ TestApp/lib/TestFunctionGroups/Autocomplete.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using nac.Forms;
+
+namespace TestApp.lib.TestFunctionGroups;
+
+public class Autocomplete
+{
+    public static void StaticStringList(Form f)
+    {
+        var items = new ObservableCollection<string>(new[]
+        {
+            "New York",
+            "Chicago",
+            "Cupertino",
+            "Sacramento"
+        });
+
+        f.Model["items"] = items;
+
+        f.Autocomplete<string>(itemSourceModelName: "items",
+                selectedItemModelName: "i")
+            .HorizontalGroup(h =>
+            {
+                h.Text("Selected: ")
+                    .TextFor("i");
+            });
+    }
+    
+    
+    
+    
+    
+}
