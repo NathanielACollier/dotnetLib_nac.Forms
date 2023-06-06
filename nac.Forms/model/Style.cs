bf50890fc63a0452436070c442fc091929d05ca1
Nathaniel A Collier <drxs3v3n@gmail.com>
2023-06-05 19:58:12 -0500

    Move optional to a library

--- nac.Forms/model/Style.cs
+++ nac.Forms/model/Style.cs
@@ -1,27 +1,28 @@
 using System;
 using System.Collections.Generic;
 using Avalonia.Media;
+using nac.utilities;
 
 namespace nac.Forms.model;
 
 public class Style
 {
-    public model.Optional<Avalonia.Media.Color> foregroundColor { get; set; } = new Optional<Color>();
-    public model.Optional<Avalonia.Media.Color> backgroundColor { get; set; } = new Optional<Color>();
-    public model.Optional<int> height { get; set; } = new Optional<int>();
+    public Optional<Avalonia.Media.Color> foregroundColor { get; set; } = new Optional<Color>();
+    public Optional<Avalonia.Media.Color> backgroundColor { get; set; } = new Optional<Color>();
+    public Optional<int> height { get; set; } = new Optional<int>();
 
-    public model.Optional<int> width { get; set; } = new Optional<int>();
+    public Optional<int> width { get; set; } = new Optional<int>();
 
-    public model.Optional<string> isVisibleModelName { get; set; } = new Optional<string>();
-    public model.Optional<string> isHiddenModelName { get; set; } = new Optional<string>();
+    public Optional<string> isVisibleModelName { get; set; } = new Optional<string>();
+    public Optional<string> isHiddenModelName { get; set; } = new Optional<string>();
 
 
     public Action<Form> contextMenu { get; set; }
 
-    public model.Optional<IEnumerable<model.MenuItem>> contextMenuItems { get; set; } = new Optional<IEnumerable<MenuItem>>();
+    public Optional<IEnumerable<model.MenuItem>> contextMenuItems { get; set; } = new Optional<IEnumerable<MenuItem>>();
 
     public Action<Form> Tooltip { get; set; }
-    public model.Optional<string> TooltipText { get; set; } = new Optional<string>();
+    public Optional<string> TooltipText { get; set; } = new Optional<string>();
 
 
     public static implicit operator Style(string cssText)
