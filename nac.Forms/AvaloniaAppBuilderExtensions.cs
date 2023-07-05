d6129aef268e87bc95326aa57640ca8921c80b8c
Nathaniel A Collier <drxs3v3n@gmail.com>
2023-07-05 07:42:09 -0500

    This is possibly the way you do the app builder now

--- nac.Forms/AvaloniaAppBuilderExtensions.cs
+++ nac.Forms/AvaloniaAppBuilderExtensions.cs
@@ -5,12 +5,10 @@ namespace nac.Forms
 {
     public static class AvaloniaAppBuilderExtensions
     {
-        public delegate void ConfigureAppBuilder<TAppBuilder>(TAppBuilder appBuilder)
-            where TAppBuilder : Avalonia.Controls.AppBuilderBase<TAppBuilder>, new();
+        public delegate void ConfigureAppBuilder(Avalonia.AppBuilder appBuilder);
         
-        public static Form NewForm<TAppBuilder>(this TAppBuilder appBuilder,
-            ConfigureAppBuilder<TAppBuilder> beforeAppBuilderInit=null)
-            where TAppBuilder : AppBuilderBase<TAppBuilder>, new()
+        public static Form NewForm(this Avalonia.AppBuilder appBuilder,
+            ConfigureAppBuilder beforeAppBuilderInit=null)
         {
             beforeAppBuilderInit?.Invoke(appBuilder);
             var builder = appBuilder
