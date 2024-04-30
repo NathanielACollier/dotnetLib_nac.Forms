573ca23032a6576f635705b79553d2855765d227
Nathaniel A Collier <drxs3v3n@gmail.com>
2024-04-30 18:01:59 -0500

    Increment Version + Pull over the new version of the build/push package script

--- nac.Forms/PublishNugetToNugetOrg.ps1
+++ nac.Forms/PublishNugetToNugetOrg.ps1
@@ -1,20 +1,19 @@
-﻿$buildConfigurationName = "Debug"
+﻿
+$buildConfigurationName = "Release"
 
+$projectFileInfo = Get-ChildItem -Path $PSScriptRoot | where { $_.Extension -eq ".csproj"} | select -First 1
+Write-Host "Project file " $projectFileInfo.FullName
+& dotnet @("publish", $projectFileInfo.FullName,"-c", $buildConfigurationName)
 
-$scriptParentFolderPath = [System.IO.Path]::GetDirectoryName( $MyInvocation.InvocationName )
-
-$buildPath = [system.io.path]::Combine( $scriptParentFolderPath, "bin", $buildConfigurationName)
-$nugetPackages = Get-ChildItem $buildPath -Filter *.nupkg
-$package = $nugetPackages | Sort-Object -Descending -Property CreationTIme | Select-Object -First 1
-
+$package = Get-ChildItem "$PSScriptRoot\bin\$buildConfigurationName\" -Filter *.nupkg | sort-object -Descending -Property LastWriteTime | select -First 1
+Write-Host "Package found " $package.FullName
 $settings = (Get-Content '~/settings.json' | Out-String | ConvertFrom-Json)
 
 <#
 Example from: https://docs.microsoft.com/en-us/nuget/nuget-org/publish-a-package
 dotnet nuget push AppLogger.1.0.0.nupkg --api-key <put key here> --source https://api.nuget.org/v3/index.json
-
 #>
 
 & dotnet @("nuget","push", $package.FullName, "--api-key", $settings.nugetOrgAPIKey, "--source",
-         "https://api.nuget.org/v3/index.json" 
- )
+"https://api.nuget.org/v3/index.json"
+)
