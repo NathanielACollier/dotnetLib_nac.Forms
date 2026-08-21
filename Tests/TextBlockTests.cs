b0bbe0bc09d00dc41468a5fe53ce959d9dfb8d2e
Nathaniel Collier <nathaniel.collier@aecc.com>
2026-08-21 07:17:12 -0500

    More work testing to see if we've got the new formatting code right

--- Tests/TextBlockTests.cs
+++ Tests/TextBlockTests.cs
@@ -13,7 +13,8 @@ public class TextBlockTests
             .NewForm();
 
         f.Text("THis is a sample text.   Should be orange background and white foreground",
-                style: "background-color: #FFBA00; color: #dde9e7; font-size:14pt;")
+                style: "background-color: #FFBA00; color: #dde9e7; font-size:14pt; font-weight:bold;")
+            .Text("Normal Text formatting", style: "font-size:14pt;")
             .Display();
     }
 }
